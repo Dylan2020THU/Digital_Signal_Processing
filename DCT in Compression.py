f4a166df75964ba31a6dd34168c41650aeb59d8c
Hengxi Zhang <72500446+Dylan2020THU@users.noreply.github.com>
2021-05-11 23:40:06 +0800

    Update DCT in Compression.py
    Add function: exhibit the curve of compression loss of DCT using MSE.

--- DCT in Compression.py
+++ DCT in Compression.py
@@ -1,5 +1,5 @@
 # Discrete Fourier Transform
-# 2021-5-10
+# 2021-5-11
 # TBSI, THU
 # ZHX
 
@@ -16,18 +16,49 @@ def DCT(input, K, N):
 
     # DFT
     X = np.zeros(K)
+    # print('X',X)
 
     for k in range(K):
         # phi = sqrt(1/N), for k = 0
         if k == 0:
             for n in range(N):
-                X[k] += input[n] * np.sqrt(1/N)
+                X[k] += input[n] * np.sqrt(1 / N)
 
         # phi = sqrt(2/N) * cos((pi * k)/N *(n+1/2), for k = 1,2,...
         else:
             for n in range(N):
-                X[k] += input[n] * np.cos(2 * np.pi * k * n / N)
+                X[k] += input[n] * np.sqrt(2 / N) * np.cos((np.pi * k) / N * (n + 0.5))
     return X
+
+
+def imitate(term_num):
+    # DCT
+    # term_num = 6
+    X_k = DCT(x_n, term_num, num)  # Calculate each X[k]
+
+    # Draw the fig of input signal
+    global fig
+    fig = plt.figure()
+    ax1 = fig.add_subplot(211)
+    ax1.set(xlabel='n', ylabel='x[n]')
+    ax1.plot(n, x_n, label='x[n]')  # draw x[n]
+    ax1.scatter(np.arange(term_num), X_k, c='r')  # draw X[k]
+
+    x_head = np.zeros((term_num, num))  # Apply for r.v. space
+    x_head_total = np.zeros(num)
+    for k in range(term_num):
+        if k == 0:
+            x_head[k] = X_k[k] * np.sqrt(1 / num)
+        else:
+            x_head[k] = X_k[k] * np.sqrt(2 / num) * np.cos((np.pi * k) / num * (n + 0.5))
+        x_head_total += x_head[k]
+        ax1.plot(n, x_head[k], c='g', label='basis_%s[n]' % k)
+
+    # print('x_head:', x_head)
+    ax1.plot(n, x_head_total, label='x_head[n]')
+
+    compression_loss = sum((x_head_total - x_n) * (x_head_total - x_n)) / num
+    return compression_loss
 
 
 if __name__ == '__main__':
@@ -36,18 +67,16 @@ if __name__ == '__main__':
     num = n_end - n_start
     n = np.arange(n_start, n_end)
     print(n)
-    x_n = np.exp(-n/10)  # input signal
+    x_n = np.exp(-n / 10)  # input signal
 
-    # DCT
-    term_num = 5
-    X_k = DCT(x_n, term_num, num)
+    #  Draw the curve of the compression loss using MSE
+    L = num
+    loss = np.zeros(L)
+    for l in range(L):
+        loss[l] = imitate(l)
 
-    # Draw the fig of input signal
-    fig = plt.figure()
-    ax1 = fig.add_subplot(111)
-    ax1.set(xlabel='n', ylabel='x[n]')
-    ax1.plot(n, x_n, label='x[n]')  # draw x[n]
-    ax1.scatter(np.arange(term_num), X_k, c='r')  # draw X[k]
+    ax2 = fig.add_subplot(212)
+    ax2.plot(np.arange(L), loss)
 
     plt.legend()
     plt.show()
